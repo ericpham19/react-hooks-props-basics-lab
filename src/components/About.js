6896ed4f6030faa0217c1248cdf2d6058939040c
Duy Pham <ericpham19@outlook.com>
2022-08-17 14:20:48 +1000

    finished

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-function About() {
+function About(props) {
+  const bio = props.bio;
+  if ( bio === ""  || typeof bio === "undefined") return null;
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <h3>Links</h3>
+      <a href = {props.github}>https://github.com/liza</a>
+      <a href = {props.linkedin}>https://www.linkedin.com/in/liza/</a>
+      
+  
     </div>
   );
 }
